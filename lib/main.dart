adaa638479be21b0bca309d160a428e5788171a8
Yuta Kawabe <yuyu3165@gmail.com>
2023-03-29 17:12:19 +0900

    combine two tween, use CurvedAnimation

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,36 @@
+// #docregion ShakeCurve
+import 'dart:math';
+
+// #enddocregion ShakeCurve
 import 'package:flutter/material.dart';
 
 void main() => runApp(const LogoApp());
 
-// #docregion LogoWidget
-class LogoWidget extends StatelessWidget {
-  const LogoWidget({super.key});
+// #docregion diff
+class AnimatedLogo extends AnimatedWidget {
+  const AnimatedLogo({super.key, required Animation<double> animation})
+      : super(listenable: animation);
 
-  // Leave out the height and width so it fills the animating parent
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      child: const FlutterLogo(),
-    );
-  }
-}
-// #enddocregion LogoWidget
-
-// #docregion GrowTransition
-class GrowTransition extends StatelessWidget {
-  const GrowTransition(
-      {required this.child, required this.animation, super.key});
-
-  final Widget child;
-  final Animation<double> animation;
+  // Make the Tweens static because they don't change.
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
+  static final _sizeTween = Tween<double>(begin: 0, end: 300);
 
   @override
   Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
     return Center(
-      child: AnimatedBuilder(
-        animation: animation,
-        builder: (context, child) {
-          return SizedBox(
-            height: animation.value,
-            width: animation.value,
-            child: child,
-          );
-        },
-        child: child,
+      child: Opacity(
+        opacity: _opacityTween.evaluate(animation),
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          height: _sizeTween.evaluate(animation),
+          width: _sizeTween.evaluate(animation),
+          child: const FlutterLogo(),
+        ),
       ),
     );
   }
 }
-// #enddocregion GrowTransition
 
 class LogoApp extends StatefulWidget {
   const LogoApp({super.key});
@@ -51,7 +39,6 @@ class LogoApp extends StatefulWidget {
   State<LogoApp> createState() => _LogoAppState();
 }
 
-// #docregion print-state
 class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   late Animation<double> animation;
   late AnimationController controller;
@@ -59,25 +46,28 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
+    // #docregion AnimationController, tweens
     controller =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    // #enddocregion AnimationController, tweens
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
     controller.forward();
   }
-  // #enddocregion print-state
 
   @override
-  Widget build(BuildContext context) {
-    return GrowTransition(
-      animation: animation,
-      child: const LogoWidget(),
-    );
-  }
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
 
   @override
   void dispose() {
     controller.dispose();
     super.dispose();
   }
-// #docregion print-state
 }
+// #enddocregion diff
